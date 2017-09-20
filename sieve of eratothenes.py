c892fc9564809549ede278a42d65fbe6d101ab13
Samman Bikram Thapa <8939811+sammanthp007@users.noreply.github.com>
2017-09-19 23:03:21 -0400

    Update sieve of eratothenes.py

--- sieve of eratothenes.py
+++ sieve of eratothenes.py
@@ -1,19 +1,13 @@
 def number_of_prime_numbers_in(n):
     non_primes = []
-    num_of_primes = 0
+    primes = []
     for num in range(2, n + 1):
-        if num in non_primes:
-            continue
-        add_multiples_to_non_primes(non_primes, num, n)
-        num_of_primes += 1
-    return num_of_primes
+        if num not in non_primes:
+            primes.append(num)
+            for j in xrange(num * num, n + 1, num):
+                non_primes.append(j)
+    return len(primes)
 
-def add_multiples_to_non_primes(non_primes, num, n):
-    multiply_with = 1
-    while num * multiply_with <= n:
-        non_primes.append(num * multiply_with)
-        multiply_with += 1
-
-print (number_of_prime_numbers_in(1000))
+print (number_of_prime_numbers_in(100000))
 
 # todo: create a dictionary of already calculated nums.. decrease the time
