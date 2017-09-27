b1ddfc85973bab929f20ccdfa696ae670c8dbb63
sammanthp007 <samman.thapa@bison.howard.edu>
2017-09-26 23:52:15 -0400

    finale

--- test/sieve.c
+++ test/sieve.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// int NUM = 1000000;
-int NUM = 100;
+int NUM = 1000000;
 
 int main() {
     int *prime; 
     prime = malloc(sizeof(int) * NUM);
-    // int prime[NUM+1];
 
     for (int i = 2; i <= NUM; i++) {
         prime[i] = 1;
